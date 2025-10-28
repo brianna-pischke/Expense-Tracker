a2d8cfe06fa3ff2fa62e33dae37c21588412d72b
brianna-pischke <151676326+brianna-pischke@users.noreply.github.com>
2025-10-27 20:08:19 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const connectDB = require('./config/db');
 
 const app = express();
 
@@ -10,6 +11,8 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Backend server is running');
 });
+
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log('Server running on port ${PORT}'));
