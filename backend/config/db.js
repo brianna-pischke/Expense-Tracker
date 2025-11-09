7238568d3fcdfca485ea1dcd980505810f538894
BriAnna Pischke <brianna.notion@gmail.com>
2025-11-09 14:35:50 -0600

    Add backend setup and dependencies for shared MongoDB cluster

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    mongoose.connection.once('open', () => {
+      console.log('MongoDB connection established');
     });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(`Error: ${error.message}`);
+    console.error(`Error connecting to MongoDB: ${error.message}`);
     process.exit(1);
   }
 };
