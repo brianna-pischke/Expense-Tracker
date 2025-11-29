2c32abd26323d161627eeb7ad850039dc7a63b13
brianna-pischke <151676326+brianna-pischke@users.noreply.github.com>
2025-11-29 13:46:05 -0600

    Create Budget.js
    Add Budget model to backend

---
+++ backend/models/Budget.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const budgetSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  month: {
+    type: String,
+    required: true,
+  },
+}, { timestamps: true });
+
+module.exports = mongoose.model("Budget", budgetSchema);
