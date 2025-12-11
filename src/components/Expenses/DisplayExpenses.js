228524c1444cf961b1f30cb694aba8904c28d4cc
Cnuttall1227 <53721838+Cnuttall1227@users.noreply.github.com>
2025-12-10 19:41:24 -0600

    Update DisplayExpenses.js fixed typo

--- src/components/Expenses/DisplayExpenses.js
+++ src/components/Expenses/DisplayExpenses.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import "./DisplayExpenses.css";
 import Card from "../UI/Card";
@@ -6,27 +6,21 @@ import ExpensesFilter from "../ExpensesFilter/ExpensesFilter";
 import ExpensesChart from "./ExpensesChart";
 
 function DisplayExpenses(props) {
-
-  const [filterYear, setFilteredYear] = useState('2020');
-
+  const currentYear = new Date().getFullYear().toString();
+  const [filterYear, setFilteredYear] = useState(currentYear);
+  
   const filterChangeHandler = year => {
     setFilteredYear(year);  
-    // console.log("Year");
-    // console.log(year);
-  }
-
-const filteredExpenses = props.expenses_list.filter(expense => expense.date.getFullYear().toString() === filterYear);
-
+  };
+  
+  const filteredExpenses = props.expenses_list.filter(expense => expense.date.getFullYear().toString() === filterYear);
+  
   return (
     <div>
-
       <Card className="expenses">
-
         <ExpensesFilter selected={filterYear} onFilterChange={filterChangeHandler}/>
-
         <ExpensesChart expenses={filteredExpenses} />
-
-        {filteredExpenses.length === 0 && (<p>Mo expenses found.</p>)}
+        {filteredExpenses.length === 0 && (<p>No expenses found.</p>)}
         
         {
           filteredExpenses.length > 0 && 
@@ -39,8 +33,6 @@ const filteredExpenses = props.expenses_list.filter(expense => expense.date.getF
           /> 
         ))
         )}
-
-
       </Card>
     </div>
   );
