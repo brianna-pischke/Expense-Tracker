0ca30aa0a5a705c3867ac6857709fa1655bab108
Cnuttall1227 <53721838+Cnuttall1227@users.noreply.github.com>
2025-12-10 21:26:40 -0600

    Update ExpenseItem.js updated and removed use from React

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,9 @@
-import React, { useState } from "react";
-
+import React from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
-// gonna learn about states now!
-
 function ExpenseItem(props) {
-
-  // const [title, setTitle] = useState(props.title);
-
-  // const titleChangeHandler = () => {
-  //   setTitle("Updated");
-  // }
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -21,7 +11,6 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <br></br>
     </Card>
   );
 }
